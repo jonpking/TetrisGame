fa63a87e30c4896e5257149936ad44485c35aa3d
Jonathan King <jonpking@gmail.com>
2020-05-21 16:26:35 -0700

    score is updated and rows are removed when a full row is completed

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const width = 10
     let nextRandom = 0
     let timerId
+    let score = 0
 
     //The Tetrominoes
     const lTetromino = [
@@ -106,6 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
             currentPosition = 4
             draw()
             displayShape()
+            addScore()
         }
     }
 
@@ -187,7 +189,24 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 
+    //add score
+    function addScore() {
+        for (let i = 0; i < 199; i += width) {
+            const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
 
+            if (row.every(index => squares[index].classList.contains("taken"))) {
+                score += 10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove("taken")
+                    squares[index].classList.remove("tetromino")
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
 
 
 })
